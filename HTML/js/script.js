3d11f6a7ea9af82c262ab6528d8a29b0bb9c7653
NailAbdulganiev <78472358+NailAbdulganiev@users.noreply.github.com>
2021-07-23 02:30:34 +0400

    I create an adaptive menu

---
+++ HTML/js/script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    var menu = document.getElementById('menu');
+    menu.onclick = function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
+}
